96feb82475afbe3da7588d872495001d221be1bf
trizin <25263018+trizin@users.noreply.github.com>
2021-12-22 16:37:00 +0300

    ERC20 mock take `mintto` in constructor

--- contracts/test/ERC20.sol
+++ contracts/test/ERC20.sol
@@ -1,10 +1,11 @@
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-    constructor(string memory name, string memory symbol)
-        public
-        ERC20(name, symbol)
-    {
-        _mint(msg.sender, 10000 ether);
+    constructor(
+        string memory name,
+        string memory symbol,
+        address mintto
+    ) public ERC20(name, symbol) {
+        _mint(mintto, 1000000 ether);
     }
 }
