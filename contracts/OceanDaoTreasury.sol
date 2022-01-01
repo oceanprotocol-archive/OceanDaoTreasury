921c393dd2682bba2ecd1e382bcfd7d3731e037d
trizin <25263018+trizin@users.noreply.github.com>
2022-01-01 18:07:48 +0300

    Add `tryRecover` function

--- contracts/OceanDaoTreasury.sol
+++ contracts/OceanDaoTreasury.sol
@@ -44,6 +44,46 @@ contract OceanDaoTreasury is Ownable {
         emit VerifierWalletSet(verifierWallet);
     }
 
+    /**
+     * @dev Overload of {ECDSA-tryRecover} that receives the `v`,
+     * `r` and `s` signature fields separately.
+     *
+     * _Available since v4.3._
+     */
+    function tryRecover(
+        bytes32 hash,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) internal pure returns (address) {
+        // EIP-2 still allows signature malleability for ecrecover(). Remove this possibility and make the signature
+        // unique. Appendix F in the Ethereum Yellow paper (https://ethereum.github.io/yellowpaper/paper.pdf), defines
+        // the valid range for s in (301): 0 < s < secp256k1n ÷ 2 + 1, and for v in (302): v ∈ {27, 28}. Most
+        // signatures from current libraries generate a unique signature with an s-value in the lower half order.
+        //
+        // If your library generates malleable signatures, such as s-values in the upper range, calculate a new s-value
+        // with 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 - s1 and flip v from 27 to 28 or
+        // vice versa. If your library also generates signatures with 0/1 for v instead 27/28, add 27 to v to accept
+        // these malleable signatures as well.
+        if (
+            uint256(s) >
+            0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0
+        ) {
+            return address(0);
+        }
+        if (v != 27 && v != 28) {
+            return address(0);
+        }
+
+        // If the signature is valid (and not malleable), return the signer address
+        address signer = ecrecover(hash, v, r, s);
+        if (signer == address(0)) {
+            return address(0);
+        }
+
+        return signer;
+    }
+
     /*
      * @dev Withdraw tokens from the treasury.
      * @param _amount The amount of tokens to deposit.
@@ -105,7 +145,7 @@ contract OceanDaoTreasury is Ownable {
 
         require(isGrantClaimed[message] == false, "Grant already claimed"); // Check if grant has already been claimed
 
-        address signer = ecrecover(message, v, r, s);
+        address signer = tryRecover(message, v, r, s);
         require(signer == verifierWallet, "Not authorized"); // Check if the verifier wallet is the signer
 
         isGrantClaimed[message] = true; // Mark grant as claimed
